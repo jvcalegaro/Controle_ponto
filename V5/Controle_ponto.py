b7502f29a42897573ab7c2abe254972aa14af0a7
José Vitor Calegaro <j.vitorcalegaro@gmail.com>
2025-04-16 16:49:43 -0300

    quinta versao

---
+++ V5/Controle_ponto.py
@@ -0,0 +1,48 @@
+import datetime
+
+
+class dia(datetime.time):
+    def __init__(self):
+        self.turno_I_entrada = None
+        self.turno_I_saida = None
+        self.turno_II_entrada = None
+        self.turno_II_saida = None
+    
+    def set_turnos(self):
+        
+        def entrada_I():
+            input_str = (input('Entrada primeiro turno!\nInsira o horario (HH:MM): '))
+            self.turno_I_entrada = datetime.datetime.strptime(input_str, "%H:%M").time()
+
+        def saida_I():
+            input_str = (input('Saida primeiro turno!\nInsira o horario (HH:MM): '))
+            self.turno_I_saida = datetime.datetime.strptime(input_str, "%H:%M").time()
+
+        def entrada_II():
+            input_str = (input('Entrada segundo turno!\nInsira o horario: '))
+            self.turno_II_entrada = datetime.datetime.strptime(input_str, "%H:%M").time()
+
+        def saida_II():
+            input_str = (input('Saida segundo turno!\nInsira o horario: '))
+            self.turno_II_saida = datetime.datetime.strptime(input_str, "%H:%M").time()
+
+        entrada_I()
+        saida_I()
+        entrada_II()
+        saida_II()
+
+    def get_turnos(self):
+
+        return {
+            "Entrada I": self.turno_I_entrada,
+            "Saída I": self.turno_I_saida,
+            "Entrada II": self.turno_II_entrada,
+            "Saída II": self.turno_II_saida,
+        }
+        
+
+
+hoje = dia()
+
+hoje.set_turnos()
+print(hoje.get_turnos())
